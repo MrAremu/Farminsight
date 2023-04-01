8c3250c8cecb7c5706bb2525a53784b02d74d7a3
MrAremu <andrew.mendoza.aremu@gmail.com>
2023-04-01 22:01:10 +0100

    navlinks changes

--- src/components/navbar/NavLinks.tsx
+++ src/components/navbar/NavLinks.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 
 function NavLinks() {
-
-return (
-<Container>
-<Navbar.Brand href="/">Farminsight</Navbar.Brand>
-<Navbar.Toggle aria-controls="responsive-navbar-nav" />
-<Navbar.Collapse id="responsive-navbar-nav">
-<Nav className="me-auto">
-<Nav.Link href="/">Home</Nav.Link>
-<Nav.Link href="/forum">Forum</Nav.Link>
-<Nav.Link href="/blogpost">Blogpost</Nav.Link>
-<Nav.Link href="/blog">Blog</Nav.Link>
-<Nav.Link href="/Gallery">Gallery</Nav.Link>
-<Nav.Link href="/About Us">AboutUs</Nav.Link>
-</Nav>
-</Navbar.Collapse>
-</Container>
-)
+  return (
+    <Container>
+      <Navbar.Brand as={Link} to="/">Farminsight</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/forum">Forum</Nav.Link>
+          <Nav.Link as={Link} to="/blogpost">Blogpost</Nav.Link>
+          <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+          <Nav.Link as={Link} to="/gallery">Gallery</Nav.Link>
+          <Nav.Link as={Link} to="/aboutus">About Us</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  );
 }
 
 export default NavLinks;
--- src/components/navbar/NavbarComp.tsx
+++ src/components/navbar/NavbarComp.tsx
@@ -12,29 +12,30 @@ import NavContainer from './NavLinks';
 import Footer from '../Footer';
 import Gallery from '../Gallery';
 import Header from '../Header';
-
+import AboutUs from '../AboutUs'; // import the AboutUs component
 
 export default class NavbarComp extends Component {
   render() {
     return (
       <Router>
         <div>
-        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-          <NavContainer />
-        </Navbar>
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <NavContainer />
+          </Navbar>
         </div>
         <div>
-        <Routes>
-          <Route path="/" element={<Homepage />} />
-          <Route path="/BlogPost" element={ <BlogPost title={''} content={''}/>} />
-          <Route path="/Forum" element={<Forum />} />
-          <Route path="/Gallery" element={<Gallery/>} />
-          <Route path="/Header" element={<Header />} />
-          <Route path="/Footer" element={<Footer />} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/BlogPost" element={<BlogPost title={''} content={''} />} />
+            <Route path="/Forum" element={<Forum />} />
+            <Route path="/Gallery" element={<Gallery/>} />
+            <Route path="/Header" element={<Header />} />
+            <Route path="/Footer" element={<Footer />} />
+            <Route path="/AboutUs" element={<AboutUs />} /> {/* add the AboutUs route */}
+          </Routes>
         </div>
         <Footer />
       </Router>
-    )
+    );
   }
 }
